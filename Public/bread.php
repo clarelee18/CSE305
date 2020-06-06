fc5bdd5ab2b86acbfc01dac8e52438879b9e44d5
clarelee18 <63967473+clarelee18@users.noreply.github.com>
2020-06-06 13:26:46 +0900

    Add category pages

---
+++ Public/bread.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1"/>
+<title>Bread & Snacks</title>
+</head>
+
+<body>
+
+
+</body>
+</html>
---
+++ Public/meat.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1"/>
+<title>Meat & Poultry</title>
+</head>
+
+<body>
+
+
+</body>
+</html>
