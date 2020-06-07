e91972f882ff1792ff63b31119029592d113ad05
clarelee18 <63967473+clarelee18@users.noreply.github.com>
2020-06-07 13:20:06 +0900

    fix log in/out options

--- Public/processed.php
+++ Public/processed.php
@@ -1,11 +1,20 @@
 <?php
 // Initialize the session
 session_start();
- 
+
 // Check if the user is logged in, if not then redirect him to login page
+if(isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] == true){
+?>  <p>
+        <a href="logout.php" class="btn btn-danger">Logout</a>
+    </p>
+<?php
+}
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-    header("location: login.php");
-    exit;
+?>
+    <p>
+    <a href="login.php" class="btn btn-danger">Login</a>
+    </p>
+<?php
 }
 ?>
 
