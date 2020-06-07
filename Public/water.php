a9093cc7c94b6774234582508f74a2a75b931c64
clarelee18 <63967473+clarelee18@users.noreply.github.com>
2020-06-07 13:24:40 +0900

    fix log in/out options

--- Public/water.php
+++ Public/water.php
@@ -1,22 +1,4 @@
-<?php
-// Initialize the session
-session_start();
 
-// Check if the user is logged in, if not then redirect him to login page
-if(isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] == true){
-?>  <p>
-        <a href="logout.php" class="btn btn-danger">Logout</a>
-    </p>
-<?php
-}
-if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-?>
-    <p>
-    <a href="login.php" class="btn btn-danger">Login</a>
-    </p>
-<?php
-}
-?>
 
 <!DOCTYPE html>
 <html lang="en">
