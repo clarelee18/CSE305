f854823fd6021b67f2b931816f23b8924561593f
Kelly Peng <mingyu.peng@stonybrook.edu>
2020-06-09 00:27:39 +0900

    Update order.php

--- public/order.php
+++ public/order.php
@@ -1,7 +1,12 @@
 <?php
 session_start();
-?>
 
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+?>
 <?php
 $conn = mysqli_connect('localhost','root','root','fooddb');
 if (mysqli_connect_errno()){
@@ -14,11 +19,26 @@ if (mysqli_connect_errno()){
 
 <head>
     <title>Order</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
 
 <body>
-    <p><a href="index.php"><h2>Homepage</h2></a></p>
-    <p><a href="welcome.php" class="btn btn-primary">My Page</a></p>
+    <nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="index.php">Online Grocery</a>
+        </div>
+        <ul class="nav navbar-nav">
+            <li class="active"><a href="index.php">Home</a></li>
+            <li><a href="welcome.php">My Page</a></li>
+         </ul>
+        <ul class="nav navbar-nav navbar-right">
+            <li><a href="logout.php"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+        </ul>
+    </div>
+     </nav>
     <!--adding the details of the order: list, payment method, payment time, address....-->
 </body>
 </html>
