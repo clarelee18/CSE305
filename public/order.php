79a88e3e7ad21f5ea49197c72013d7b670588043
Kelly Peng <mingyu.peng@stonybrook.edu>
2020-06-08 17:21:07 +0900

    update checkout and order page

---
+++ public/order.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Order</title>
+</head>
+
+<body>
+    <p><a href="index.php"><h2>Homepage</h2></a></p>
+    <p><a href="welcome.php" class="btn btn-primary">My Page</a></p>
+    <!--adding the details of the order: list, payment method, payment time, address....-->
+    
+</body>
+</html>
